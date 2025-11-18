19b7b081de65fc60140c5a0d3d3c63ff71c74c5e
Kiran31patel <kiranpatelbpprasanna@gmail.com>
2025-11-18 22:28:00 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,12 @@ from setuptools import setup, find_packages
 from typing import List
 
 
-def get_requirements() -> List[str]:
-    """ 
-    This function will return the list of requirements 
-    mentioned in the requirements.txt file 
-    """
-    try:
-        with open('requirements.txt') as req_file:
-            requirements = req_file.readlines()
-            requirements = [req.strip() for req in requirements if req.strip()]
-        return requirements
-    except FileNotFoundError:
-        return []
+def get_requirements(file_path:str)->List[str]:
+    """This function will return the list of requirements"""
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
